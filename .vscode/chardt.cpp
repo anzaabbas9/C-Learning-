a3600fc09c5c18069e21a55446acd419e7e51a72
unknown <anzaabbas9@gmail.com>
2026-03-03 11:33:39 +0500

    c_code

--- .vscode/chardt.cpp
+++ .vscode/chardt.cpp
@@ -6,8 +6,8 @@ int main(){
     char ch='a';
    // cout<<(int)ch<<endl;
     cout<<ch<<endl;
-    cout<<SCHAR_MIN<<endl;
-    cout<<SCHAR_MAX<<endl;
-    cout<<__SCHAR_WIDTH__<<endl;
+   // cout<<SCHAR_MIN<<endl;
+    //cout<<SCHAR_MAX<<endl;
+    //cout<<__SCHAR_WIDTH__<<endl;
     return 0;
 }
