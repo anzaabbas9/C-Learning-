8f8d5d8f6811a688685634674484c4d3fac7b3b4
unknown <anzaabbas9@gmail.com>
2026-03-04 10:53:20 +0500

    bool_c++

---
+++ booldt.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool x=true;
+    //int x=54;
+    //float y=45.56;
+   // float z=x+y-true+false;
+    cout<<x<<endl;
+   // cout<<z<<endl;
+    cout<<sizeof(bool);
+}
