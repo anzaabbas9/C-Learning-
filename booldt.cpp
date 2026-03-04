224baca4677f9a3b017e2ad9d02ac6a5e2d338d3
unknown <anzaabbas9@gmail.com>
2026-03-04 14:39:26 +0500

    dt code

--- booldt.cpp
+++ booldt.cpp
@@ -2,10 +2,15 @@
 using namespace std;
 int main(){
     bool a=true;
+    void *ptr;
     int x=54;
-    float y=45.56;
-    float z=x+y-true+false;
+    ptr=&x;
+    //float y=45.56;
+   // float z=x+y-true+false;
     cout<<a<<endl;
-    cout<<z<<endl;
+    cout<<*(int*)ptr<<endl;
+    cout<<ptr<<endl;
+
+    //cout<<z<<endl;
     cout<<sizeof(bool);
 }
