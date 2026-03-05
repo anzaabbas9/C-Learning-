8fe950d799d2c898a5f9c93a2f5d5d1db78bcee0
unknown <anzaabbas9@gmail.com>
2026-03-05 15:54:24 +0500

    c

---
+++ cast.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=45;
+    float a;
+    a=4.5;
+    cout<<"int a ="<<a<<endl;
+    cout<<"float a="<<a;
+    
+
+}
---
+++ fl.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    float a=1.5f;
+    double b =1.1156;
+    double sum ,pro;
+    sum=a+b;
+    pro=a*b;
+    cout<<sum<<endl;
+     cout<<pro<<endl;
+
+}
