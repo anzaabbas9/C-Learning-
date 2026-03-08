130ac521cd848127ad4eda5d61c46fb55709c239
unknown <anzaabbas9@gmail.com>
2026-03-08 16:20:55 +0500

    calculater code

--- num.cpp
+++ num.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
-using namespace std;
-int main(){
+//#include<iostream>
+//using namespace std;
+
     /*int a=15;
     if(a%2==0)
     cout<<"even num";
@@ -8,13 +8,41 @@ int main(){
     cout<<"odd num";
     
 */
-int a,b,num;
+/*int a,b,num;
 a=2;
 b=3;
 cout<<a<<" "<<b<<endl;
 num=a;
 a=b;
-b=num;
-cout<<a<<" "<<b;
-
+b=num;S
+cout<<a<<" "<<b;*/
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+char op;
+a=4;
+b=5;
+cout<<"enter an op(+,-,*,/,%): ";
+cin>>op;
+switch(op)
+{
+    case'+':
+    cout<<a+b<<endl;
+    break;
+   case'-':
+    cout<<a-b<<endl;
+    break;
+    case'*':
+    cout<<a*b<<endl;
+    break;
+    case'/':
+    cout<<a/b<<endl;
+    break;
+    case'%':
+    cout<<a%b<<endl;
+    default:
+    cout<<"invalid operater ";
 }
+    return 0;
+    }
