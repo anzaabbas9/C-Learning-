3855ede81bead66e16ec77124085b68885123bff
unknown <anzaabbas9@gmail.com>
2026-03-10 16:38:01 +0500

    unary code

---
+++ .vscode/increment.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=3,y;
+    y=++x + ++x;
+    cout<<"x= "<<"y= "<<x<<y<<endl;
+    
+}
--- presidence.cpp
+++ presidence.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a=(1,2);
-    int a=(cout<<"hello",2,3,4);
-    cout<<"a="<<endl;
-}
+//    int a=(1,2);
+//    int a=(cout<<"hello",2,3,4);
+    // cout<<"a="<<a<<endl;
+
+int a=10;
+int b=20;
+int c=a*b/c<<a|b%a^b&&a;
+cout<<"c="<<c;}
