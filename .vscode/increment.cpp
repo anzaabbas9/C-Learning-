401ebc66fcc78ad235d91f67e0503e4c61240eaf
unknown <anzaabbas9@gmail.com>
2026-03-10 17:30:15 +0500

    associatitvity code

--- .vscode/increment.cpp
+++ .vscode/increment.cpp
@@ -3,8 +3,10 @@ using namespace std;
 int main(){
     //int x=3,y;
    // y=++x - ++x + ++x;
-   int x=5,y;
-   y=x++ + ++x +1 + --x + x--;
-    cout<<"x= "<<x<<"  y= "<<y<<endl;
+   int x=10,y=20,z;
+   //z=x++ + y++ +x +y + ++x + ++y;
+    z=x-- - x + ++x + --y - ++y + --x - y-- + ++x -  y++;
+   //y=x++ + ++x +1 + --x + x--;
+    cout<<"x= "<<x<<"  y= "<<y<<" "<<"z="<<z<<endl;
 
 }
