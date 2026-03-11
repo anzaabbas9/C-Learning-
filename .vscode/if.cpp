c47290fcf8f53c0ab620c6f04cb7c4f723d9bf79
unknown <anzaabbas9@gmail.com>
2026-03-11 12:46:58 +0500

    change code

--- .vscode/if.cpp
+++ .vscode/if.cpp
@@ -4,9 +4,18 @@ int main(){
     int money;
     cout<<"enter money you have:";
     cin>>money;
-    if(money>=1200)
-    cout<<"it's enough";
+    /*if(0)
+    cout<<"it's enough"<<endl;
     cout<<"insufficient amount";
-    return 0;
+    return 0;*/
+    if(100)
+    {
+        cout<<"it's good "<<endl;
+        cout<<"we both have nothing"<<endl;
+    }
+    else
     
+        cout<<"let,s go to cofee shop"<<endl;
+    cout<<"take some cofee"<<endl;
+    cout<<"it's time to enjoy";
 }
