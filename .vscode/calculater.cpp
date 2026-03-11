b826368112ff0d5c6cc396a3be3a1c425a1637c5
unknown <anzaabbas9@gmail.com>
2026-03-11 06:33:31 +0500

    change code

--- .vscode/calculater.cpp
+++ .vscode/calculater.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 class Calculater
 {
@@ -6,66 +7,100 @@ private:
 double a,b;
 double result;
 public:
- double add(){
-    a=10;
-    b=20;
-    result=a+b;
-    return result; 
+void input(){
+   cout<<"enter a:";
+   cin>>a;
+   cout<<"enter b:";
+   cin>>b;
+}
+double add(){
+result=a+b;
+return result;
  }
  double sub(){
-    a=10;
-    b=20;
-    result =a+b;
+   
+   
+    result =a-b;
     return result;
  }
  double mul(){
-    a=12;
-    b=6;
+    
+   
     result=a*b;
    return result;
 
  }
 double  div(){
-a=10;
-b=20;
-result=a/b;
+if (b!=0){
+result=a/b;}
+else {
+cout<<"division not possible";
+return 0;}
 return result;
 }
+double mod(){
+  
+result=(int)a%(int)b;
+return result;
+}
+double square(){
+   result=a*a;
+   return result;
+}
+
 };
 int main()
 {
     Calculater c1;
     int choice;
+   //print menu only once
     cout<<"****menu****"<<endl;
-    cout<<"enter 1 for add"<<endl;
-    cout<<"enter 2 for subtract"<<endl;
-    cout<<"enter 3 for multiplication"<<endl;
-    cout<<"enter 4 for divide"<<endl;
-    cout<<"enter 5 for exit";
+    cout<<" 1.add"<<endl;
+    cout<<"2.subtract"<<endl;
+    cout<<"3. multiplication"<<endl;
+    cout<<"4.divide"<<endl;
+    cout<<"5.modulus"<<endl;
+    cout<<"6.square"<<endl;
+    cout<<"7. exit"<<endl;
+     //run each case
+     while(true){ 
     cout<<"enter choice:"<<endl;
     cin>>choice;
+   if(choice==7){
+      cout<<"program ends";
+      break;
+   }
+    c1.input();
     switch(choice){
-        case'1':
-        c1.add();
-        cout<<"sum is :"<<c1.add();
+        case 1:
+        cout<<"sum is :"<<c1.add()<<endl;
         break;
-        case'2':
-        c1.sub();
-        cout<<"sub is :"<<c1.sub();
+
+        case 2:
+        cout<<"sub is :"<<c1.sub()<<endl;
         break;
-        case'3':
-        c1.mul();
-        cout<<"mul is :"<<c1.mul();
+
+        case 3:
+        cout<<"mul is :"<<c1.mul()<<endl;
         break;
-        case '4':
-        c1.div();
-        cout<<"div is :"<<c1.div();
+
+        case 4:
+        cout<<"div is :"<<c1.div()<<endl;
         break;
+
+        case 5:
+        cout<<"mod is:"<<c1.mod()<<endl;
+        break;
+
+        case 6:
+        cout<<"square is :"<<c1.square()<<endl;
+        break;
+
         default:
         cout<<"exit";
 
 
-    }
+    }}
 return 0;
 }
 
