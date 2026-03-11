9d4a88149a25dc5c79c54f7cfbd3fb0551e6d141
unknown <anzaabbas9@gmail.com>
2026-03-11 12:26:59 +0500

    control structure

---
+++ .vscode/if.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int money;
+    cout<<"enter money you have:";
+    cin>>money;
+    if(money>=1200)
+    cout<<"it's enough";
+    cout<<"insufficient amount";
+    return 0;
+    
+}
