6d463b147bf851ebb70c4b586f8e581c896442e0
unknown <anzaabbas9@gmail.com>
2026-03-31 17:18:27 +0500

    codes

--- .vscode/inherit.cpp
+++ .vscode/inherit.cpp
@@ -1,70 +1,35 @@
 #include<iostream>
 using namespace std;
      //base class
-class st_addr
-{
-    private:
-    string name;
-    int phone;
+class student{
+public:
+string name;
+int rollno;
+student(){
+    cout<<"hey!i'm base class constructor"<<endl;
+}
+~student(){
+    cout<<"hey!i'm base class distructor"<<endl;
+}
+void display(string n,int r);
+   
+};
+class teacher:public student{
     public:
-    void setinfo();
-    void getinfo();
-    
+    float cgpa;
+    teacher(){
+        cout<<"hey!i'm derived class cons"<<endl;
+    }
+     ~teacher(){
+        cout<<"hey!i'm derived class distructor"<<endl;
+    }
+    void display(string n,int r,float gpa){
+        cout<<"name:"<<n<<endl;
+        cout<<"roll no:"<<r<<endl;
+        cout<<"cgpa:"<<gpa<<endl;
+    }
 };
-   //derived class
-class st_result{
-public:
-int sub1;
-int sub2;
-float sum;
-float avg;
-void aver();
-};
-class teacher:public st_addr,public st_result{
-public:
-string name="ayesha";
-int grade=17;
-void display();
-};
-  //main function
 int main(){
-    st_result s1;
     teacher t1;
-    st_addr st1;
-   
-    
-    t1.setinfo();
-    t1.getinfo();
-    t1.display();
-    t1.aver();
-    
-   
-
-    
-   
-   
-}   
-    //define member function
-void st_addr ::setinfo(){
-name="anza";
-phone=12345678;
-}
-void st_addr::getinfo(){
-cout<<"name:"<<name<<endl;
-cout<<"phone:"<<phone<<endl;
-}
-void st_result::aver(){
-cout<<"enter marks of 2 sub:";
-cin>>sub1>>sub2;
-sum=sub1+sub2;
-avg=sum/2;
-}
-void teacher::display(){
-    cout<<"name of teacher:"<<name<<endl;
-    cout<<"grade:"<<grade<<endl;
-   
-    
-
-
-
+    t1.display("anza",1007,3.9);
 }
