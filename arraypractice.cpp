0f7302972aca63a9c1591c83f96083e7d1b0b3f2
anzaabbas9 <anzaabbas9@gmail.com>
2026-04-10 17:12:38 +0500

    array practice codes

--- arraypractice.cpp
+++ arraypractice.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a[15]={10,0,0,0,0,0,0,0,0,0,0,0,0,150};
+    int a[15]={0};
+    a[0]={10};
+    a[14]={150};
     for(int i=0;i<15;i++)
     cout<<a[i]<<endl;
 }
