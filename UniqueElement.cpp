ad26294bfcfa937b6d8a23cbf538cf258c28299a
anzaabbas9 <anzaabbas9@gmail.com>
2026-05-04 23:31:01 +0500

    made the duplication checking number program

--- UniqueElement.cpp
+++ UniqueElement.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int i;
-    int num=0;
-    int arr[5]={2,1,1,3,0};
-    for(int i=0;i<5;i++){
-         num=num^arr[i];
-       
-
+    int num = 0;
+    int arr[5] = {2, 1, 1, 3, 0};
+    for (int i = 0; i < 5; i++)
+    {
+        num = num ^ arr[i];
     }
-    cout<<num<<endl;
+    cout << num << endl;
 }
---
+++ test.cpp
@@ -0,0 +1,20 @@
+// Que No 1
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5];
+    for (int i = 0; i < 5; i++)
+    {
+        arr[i] = i;
+        cout << arr[i];
+    }
+}
+    */
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 0;
+    cout << "" || 0;
+}
